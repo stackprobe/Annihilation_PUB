c5acd0ad69ee65cda5532b4e73bc5848f5f0938d
stackprobe <stackprobe@gmail.com>
2026-05-30 05:17:46 +0900

    add:3 mod:2 del:0 mv:0 +511 -6 2026/05/30 05:17:46 (+22:14:38)

--- import.bat
+++ import.bat
@@ -2,5 +2,6 @@ ROBOCOPY ..\Annihilation\Game .\Game /MIR
 ROBOCOPY ..\Annihilation\LiteLauncher .\LiteLauncher /MIR
 ROBOCOPY ..\Annihilation\story .\story /MIR
 ROBOCOPY ..\Annihilation\doc .\doc /MIR
+COPY     ..\Annihilation\AGENTS.md .
 
 TIMEOUT 2
